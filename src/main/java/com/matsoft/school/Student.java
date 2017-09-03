e017191bfb34473d06e1d9dc5f55d4dbaa0155ef
user <user@users-MacBook-Pro.local>
2017-09-03 14:51:38 +0100

    I added variables to Student Class

--- src/main/java/com/matsoft/school/Student.java
+++ src/main/java/com/matsoft/school/Student.java
@@ -4,4 +4,31 @@ package com.matsoft.school;
  * Created by user on 03/09/2017.
  */
 public class Student {
+    private String name;
+    private String surname;
+    private String lesson;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(String lesson) {
+        this.lesson = lesson;
+    }
 }
