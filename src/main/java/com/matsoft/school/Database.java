4ac0013e29cae27c5428b9f07fdf21ae8823eef4
user <user@users-MacBook-Pro.local>
2017-09-03 15:29:22 +0100

    I created new class, it's FileDB. I implement from Database Interface

--- src/main/java/com/matsoft/school/Database.java
+++ src/main/java/com/matsoft/school/Database.java
@@ -4,4 +4,6 @@ package com.matsoft.school;
  * Created by user on 03/09/2017.
  */
 public interface Database {
+    public void claculateAvarage(Student student);
+    public void calculateNormal(Student student);
 }
